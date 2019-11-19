7885b234e14b6fc268de99a691d2cdc69fa8ead4
pku-hjw <huangjiawei@pku.edu.cn>
2019-11-19 18:06:16 +0800

    包列表显示1

--- paclist/src/main/java/com/owlaser/paclist/controller/PacController.java
+++ paclist/src/main/java/com/owlaser/paclist/controller/PacController.java
@@ -1,8 +1,10 @@
 package com.owlaser.paclist.controller;
 import com.owlaser.paclist.entity.Dependency;
+import com.owlaser.paclist.service.PacService;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,9 @@ import java.util.Scanner;
 
 @Controller
 public class PacController {
+
+    @Autowired
+    private PacService pacService;
 
     @GetMapping(value = "/")
     public String show(){
@@ -39,7 +44,7 @@ public class PacController {
             Files.write(path, bytes);
             model.addAttribute("messages", "文件上传成功！开始扫描");
             ArrayList<Dependency> dependenciesList = new ArrayList<>();
-            ScanPac(path1 + file.getOriginalFilename(), dependenciesList);
+            pacService.ScanPac(path1 + file.getOriginalFilename(), dependenciesList);
             model.addAttribute("dependenciesList", dependenciesList);
         }
         catch (IOException e){
@@ -53,49 +58,4 @@ public class PacController {
         return "uploadStatus";
     }
 
-    public static void ScanPac(String filepath, List<Dependency> dependenciesList) {
-        SAXReader reader = new SAXReader();
-        File pom = new File(filepath);
-        try {
-            // 通过reader对象的read方法加载xml文件 ，获取docuement对象
-            Document document = reader.read(pom);
-            // 通过document对象获取根节点root
-            Element root = document.getRootElement();
-            if(root.element("dependencyManagement") != null){
-                Element dependencyManagement = root.element("dependencyManagement").element("dependencies");
-                GetDependencies(dependencyManagement, dependenciesList);
-            }
-            Element dependencies = root.element("dependencies");
-            GetDependencies(dependencies, dependenciesList);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //得到pom文件里的依赖包信息
-    public static void GetDependencies(Element parent, List<Dependency> dependenciesList){
-        for(Iterator<Element> it = parent.elementIterator("dependency"); it.hasNext();)
-        {
-            Element element=it.next();
-            Iterator itt = element.elementIterator();
-            Dependency dependency = new Dependency();
-            while(itt.hasNext()){
-                Element value = (Element) itt.next();
-                if(value.getName().equals("artifactId")){
-                    dependency.setArtifactId(value.getStringValue());
-                }
-                else if (value.getName().equals("version") && !value.getStringValue().equals("${project.version}")){
-                    dependency.setVersion(value.getStringValue());
-                }
-                else if(value.getName().equals("groupId")){
-                    dependency.setGroupId(value.getStringValue());
-                }
-
-            }
-            if(dependency.getArtifactId() != null && dependency.getVersion() != null){
-                dependenciesList.add(dependency);
-            }
-        }
-        return;
-    }
 }
--- paclist/src/main/java/com/owlaser/paclist/service/PacService.java
+++ paclist/src/main/java/com/owlaser/paclist/service/PacService.java
@@ -1,4 +1,60 @@
 package com.owlaser.paclist.service;
 
+import com.owlaser.paclist.entity.Dependency;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
 public class PacService {
+    public static void ScanPac(String filepath, List<Dependency> dependenciesList) {
+        SAXReader reader = new SAXReader();
+        File pom = new File(filepath);
+        try {
+            // 通过reader对象的read方法加载xml文件 ，获取docuement对象
+            Document document = reader.read(pom);
+            // 通过document对象获取根节点root
+            Element root = document.getRootElement();
+            if(root.element("dependencyManagement") != null){
+                Element dependencyManagement = root.element("dependencyManagement").element("dependencies");
+                GetDependencies(dependencyManagement, dependenciesList);
+            }
+            Element dependencies = root.element("dependencies");
+            GetDependencies(dependencies, dependenciesList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //得到pom文件里的依赖包信息
+    public static void GetDependencies(Element parent, List<Dependency> dependenciesList){
+        for(Iterator<Element> it = parent.elementIterator("dependency"); it.hasNext();)
+        {
+            Element element=it.next();
+            Iterator itt = element.elementIterator();
+            Dependency dependency = new Dependency();
+            while(itt.hasNext()){
+                Element value = (Element) itt.next();
+                if(value.getName().equals("artifactId")){
+                    dependency.setArtifactId(value.getStringValue());
+                }
+                else if (value.getName().equals("version") && !value.getStringValue().equals("${project.version}")){
+                    dependency.setVersion(value.getStringValue());
+                }
+                else if(value.getName().equals("groupId")){
+                    dependency.setGroupId(value.getStringValue());
+                }
+
+            }
+            if(dependency.getArtifactId() != null && dependency.getVersion() != null){
+                dependenciesList.add(dependency);
+            }
+        }
+        return;
+    }
 }
